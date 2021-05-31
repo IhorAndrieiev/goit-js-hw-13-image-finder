62a45f55847258cf4c85d10806bdc94f87be6f9f
Ihor Andrieiev <andr.igorrom@gmail.com>
2021-05-31 19:03:15 +0300

    error

--- src/js/markup.js
+++ src/js/markup.js
@@ -1,12 +1,16 @@
 import imgMarkup from './../templates/photo-card.hbs';
 import refs from './refs'
 
+import { info, error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+
 function renderFotosCard(hits) {
     if (hits.length < 1) {
         refs.loadMoreBtn.disabled = true;
         error({
                 text: 'Ведите другой запрос, такого запроса нет!',
-                daily: 4000,
+                daily: 3000,
                 closerHover: true,
             });
     }
